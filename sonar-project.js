5a2e5e2bdb393a41dd1dc210e4e365343254e25e
nag003 <51114096+nag003@users.noreply.github.com>
2021-03-10 19:48:16 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.179.124.93:9000/',
+  serverUrl: 'http://13.213.11.225:9603/arjtechnologies/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
